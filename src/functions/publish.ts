b8a0ac8c9ab61b17b69579248f7aea9a4d10f2b2
Sebastian Müller <mail@sbstjn.com>
2024-12-13 17:18:28 +0100

    fix: update aws-sdk

--- src/functions/publish.ts
+++ src/functions/publish.ts
@@ -1,21 +1,23 @@
-import { Handler } from "aws-lambda"
-import * as AWS from "aws-sdk"
+import { Handler } from 'aws-lambda'
+import { SNS } from 'aws-sdk'
 
-const sns = new AWS.SNS({ apiVersion: "2010-03-31" })
+const sns = new SNS({ apiVersion: '2010-03-31' })
 
 export interface HandlerEvent {
   fails: boolean
 }
 
-export const handler: Handler<HandlerEvent, boolean> = async (event) => {
+export const handler: Handler<HandlerEvent, boolean> = async event => {
   if (event.fails) {
     throw new Error('Failed on purpose')
   }
 
-  await sns.publish({
-    TopicArn: process.env.TOPIC_ARN,
-    Message: JSON.stringify(event),
-  }).promise()
+  await sns
+    .publish({
+      TopicArn: process.env.TOPIC_ARN,
+      Message: JSON.stringify(event),
+    })
+    .promise()
 
   return true
 }
